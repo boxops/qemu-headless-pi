da1e027b7baadc2badcbc498bb8325457da8d4d8
Bob <olahbalazs@outlook.com>
2020-09-03 18:43:35 +0100

    Create write-to-SD.sh

---
+++ write-to-SD.sh
@@ -0,0 +1,6 @@
+#if True, write the RasPiOS image to an SD card
+WRITEIMG=$(find /home/$USER/Downloads/*-raspios-*-lite-*.img)
+WRITESD=True
+SDPATH=/dev/sdc
+
+if [ $WRITESD = True ]; then echo "### Writing RasPiOS to SD card..." && sudo dd if=$WRITEIMG of=$SDPATH bs=4M oflag=dsync status=progress ; else echo "### Not Writing RasPiOS to SD card... Exiting Script" ; fi
