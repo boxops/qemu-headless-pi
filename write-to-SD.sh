5d9933b5a2d3a3d852eb8f76a4ad9e0691978e85
Bob <olahbalazs@outlook.com>
2020-09-08 15:18:41 +0100

    Update write-to-SD.sh

--- write-to-SD.sh
+++ write-to-SD.sh
@@ -1,7 +1,14 @@
-#if True, write the RasPiOS image to an SD card
-WRITEIMG=$(find /home/$USER/Downloads/*raspios*lite*.img)
+#Ensure to format SD card before writing RasPiOS image
+
+IMGPATH=$(readlink -f "$(find . -name "*raspios*lite*.img")")
 WRITESD=True
 SDPATH=/dev/sdc
+#Find SD card path:
+#lsblk | grep sdc
 
-if [ $WRITESD = True ]; then echo "### Writing RasPiOS to SD card..." && sudo dd if=$WRITEIMG of=$SDPATH bs=4M oflag=dsync status=progress ; else echo "### Not Writing RasPiOS to SD card... Exiting Script" ; fi
+if [ $WRITESD = True ]; then echo "### Writing RasPiOS to SD card..." && sudo dd if=$IMGPATH of=$SDPATH bs=4M oflag=dsync status=progress ; else echo "### Not Writing RasPiOS to SD card... " ; fi
 
+#unmount and power off SD card for safe removal
+sudo udisksctl unmount -b "${SDPATH}1"
+sudo udisksctl unmount -b "${SDPATH}2"
+sudo udisksctl power-off -b $SDPATH
