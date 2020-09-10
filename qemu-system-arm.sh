88ab06e7a9656963de380f02309da0a60f469ca0
Bob <olahbalazs@outlook.com>
2020-09-10 11:20:05 +0100

    Update qemu-system-arm.sh

--- qemu-system-arm.sh
+++ qemu-system-arm.sh
@@ -1,7 +1,12 @@
 DOWNLOADPATH="$(pwd)/qemu-rpi-kernel"
 GITURL=https://github.com/dhruvvyas90/qemu-rpi-kernel
 
-if [ -d $DOWNLOADPATH ]; then echo "### qemu-rpi-kernel folder exists"; else echo "### Downloading qemu-rpi-kernel git repository" && git clone $GITURL $DOWNLOADPATH; fi
+if [ -d $DOWNLOADPATH ]; then
+  echo "### qemu-rpi-kernel folder exists"
+else
+  echo "### Downloading qemu-rpi-kernel git repository"
+  git clone $GITURL $DOWNLOADPATH
+fi
 
 DRIVEPATH=$(readlink -f "$(find . -name "*raspios*lite*.img")")
 DTBPATH=$(readlink -f "$(find qemu-rpi-kernel -name "versatile-pb-buster-5.4.51.dtb")")
@@ -13,10 +18,13 @@ qemu-system-arm \
 -m 256 \
 -drive "file=$DRIVEPATH,if=none,index=0,media=disk,format=raw,id=disk0" \
 -device "virtio-blk-pci,drive=disk0,disable-modern=on,disable-legacy=off" \
--net "user,hostfwd=tcp::5022-:22" \
+-nic user,hostfwd=tcp::5022-:22 \
 -dtb $DTBPATH \
 -kernel $KERNELPATH \
 -nographic \
 -append "console=ttyAMA0 root=/dev/vda2 panic=1 rootfstype=ext4 rw" \
--net nic,id=eth0 \
 -no-reboot
+
+#-net nic,id=eth0 \
+#-netdev tap,id=mynet0,ifname=tap0,script=no,downscript=no \
+
